1b7aa20f999cbfb591b52551ddf82bec1e1d73d0
bucux <ubucux@gmail.com>
2024-04-02 23:21:28 +0200

    payment fixed

--- src/comps/pages/payment.tsx
+++ src/comps/pages/payment.tsx
@@ -4,29 +4,21 @@ import "./css/payment.css";
 import { StripeElementsOptionsMode, loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import Stripeform from '../sections/stripeform';
+import { useStoreObj } from "../../stores/storeObj";
 
-// Cette ligne permet de vous connecter à votre compte Stripe en fournissant votre clef publique
-const stripePromise = loadStripe(import.meta.env.VITE_stripeKey);
-
-const price = 5.9
-
+const stripePromise = loadStripe(import.meta.env.VITE_stripeKey) // Cette ligne permet de vous connecter à votre compte Stripe en fournissant votre clef publique
 
 const Payment = () => {
-  const options : StripeElementsOptionsMode = {
-    // Type de transaction
-    mode: "payment",
-    // Montant de la transaction
-    amount: Number((price * 100).toFixed(0)),
-    // Devise de la transaction
-    currency: "eur",
-    // On peut customiser l'apparence ici
-    appearance: {
-      /*...*/
-    },
-  };
-  return (
-    // Le composant Elements doit contenir toute notre logique de paiement
-    // On lui donner la preuve que nous sommes connectés et les options de paiement
+  
+  const price = useStoreObj(state=>state.offer!.product_price)
+
+  const options : StripeElementsOptionsMode = {    
+    mode: "payment", // Type de transaction    
+    amount: Number((price * 100).toFixed(0)), // Montant de la transaction    
+    currency: "eur", // Devise de la transaction    
+    appearance: {} // On peut customiser l'apparence ici
+  }
+  return ( // Le composant Elements doit contenir toute notre logique de paiement // On lui donner la preuve que nous sommes connectés et les options de paiement
     <Elements stripe={stripePromise} options={options}>
       <Stripeform options={options}/>
     </Elements>
